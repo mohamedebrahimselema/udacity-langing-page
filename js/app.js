01c043811053624c7d1b3be2567ecbc9c5b87927
mohamedebrahimselema <mohamed.ibrahiem5905@ci.menofia.edu.egm>
2020-12-22 11:17:08 +0200

    add  highlight the menu items

--- js/app.js
+++ js/app.js
@@ -1,86 +1,106 @@
-let unorderedList = document.getElementById("unordered-list");
-let containerCount = document.getElementsByClassName("landing__container")
-  .length;
+// selecting all sections with dataset of nav, converting the nodelist to an Array
+const sections = Array.from(document.querySelectorAll("section[data-nav]"));
+const nav = document.querySelector("#navbar__list");
+const collapse = Array.from(document.querySelectorAll("#collapse"));
+const contents = Array.from(document.querySelectorAll("#content"));
+const btn = document.querySelector("#btn");
+//looping through each section and creating an unordered list and adding a unique dataset key to each li utilizing the section id
+// using section dataset nav as the innerText of the navigation menu.
+//appending each item to the navbar menu
+//on page load; the navigation menu will be created & appended from all the sections that exist in the HTML
 
-for (i = 1; i < containerCount + 1; i++) {
-  let item = "#section" + i + "-title";
-  let itemValue = document.querySelector(item);
-  let itemText = itemValue.textContent;
-  let newLine = document.createElement("li");
-  let lineText = document.createTextNode(itemText);
-  let listItem = "sample-nav-" + i;
-  newLine.setAttribute("id", listItem);
-  newLine.setAttribute("class", "nav-list-item");
-  newLine.appendChild(lineText);
-  unorderedList.appendChild(newLine);
-
-  let itemTarget = document.getElementById("section" + i);
-  let listTarget = document.getElementById(listItem);
-  let buttonName = "section" + i + "-button";
-  let buttonToAdd = document.getElementById(buttonName);
-
-  listTarget.addEventListener("click", function() {
-    itemTarget.scrollIntoView ({
-      behavior: 'smooth'}
-    )
-    buttonToAdd.innerHTML =
-      "<button class='section-button' onclick='goToTop()'>Return to Top</button>";
+for (let x of collapse) {
+  x.addEventListener("click", (e) => {
+    for (let section of sections) {
+      // if the clicked button target parentElement id match any section id of the section array
+      if (
+        section.id == e.srcElement.parentElement.parentElement.parentElement.id
+      ) {
+        // wrapped the paragraph in a div to make the collapsing effect easier
+        for (let content of contents) {
+          // if the content parent id match the targeted section id then toggle collapse.
+          if (content.parentElement.parentElement.id == section.id) {
+            content.classList.toggle("hider");
+          }
+        }
+      }
+    }
   });
 }
-const scrollToTop = () => {
-  const scrolling = document.documentElement.scrollTop || document.body.scrollTop;
-  if (scrolling > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, scrolling - scrolling / 50);
+window.onscroll = () => {
+  if (
+    document.body.scrollTop > 250 ||
+    document.documentElement.scrollTop > 250
+  ) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
   }
 };
-
-function goToTop() {
-scrollToTop();
-
-  for (i = 1; i < containerCount + 1; i++) {
-    let buttonToDelete = document.getElementById("section" + i + "-button");
-    buttonToDelete.innerHTML = "";
+btn.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+sectionCreator = () => {
+  for (let section of sections) {
+    let item = document.createElement("li");
+    item.className = "menu__link";
+    item.dataset.nav = section.id;
+    item.innerText = section.dataset.nav;
+    nav.appendChild(item);
   }
-}
-function checkIfSectionInView() {
-  let isInViewport = function(elem) {
-    let bounding = elem.getBoundingClientRect();
-    return (
-      bounding.top <= 50 &&
-      bounding.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <=
-        (window.innerWidth || document.documentElement.clientWidth)
-    );
-  };
+};
+//locating the target by utilizing the unique dataset nav key and using javascript method scrollIntoView()
+// Clicking on an item in the navigation menu should scroll to the choosen section
+// this function is used as an event listener on the navigation menu as a click event.
+clicker = (e) => {
+  e.preventDefault();
+  let item = document.getElementById(e.target.dataset.nav);
+  item.classList.add("your-active-class");
+  item.scrollIntoView({
+    behavior: "smooth",
+    block: "end",
+    inline: "nearest",
+  });
+};
 
-  for (i = 1; i < containerCount + 1; i++) {
-    let sectionInFullView = document.getElementById("section" + i);
-
-    window.addEventListener(
-      "scroll",
-      function(event) {
-        if (isInViewport(sectionInFullView)) {
-          sectionInFullView.classList.add("your-active-class");
-        } else {
-          sectionInFullView.classList.remove("your-active-class");
-        }
-      },
-      false
-    );
+//test which section should be highlighted using .getBoundingClientRect();
+/*
+  calculating the top of the section item (by default first one) and when the next section starts to be on viewport the previous section classList will remove the highlight 
+  and the current new section will be hightlighted 
+*/
+isInViewport = () => {
+  let currentElem = sections[0]; // highlight first section on default
+  let top = 500;
+  let navMenu = Array.from(document.querySelectorAll(".menu__link"));
+  for (item of sections) {
+    let rect = item.getBoundingClientRect();
+    //returns the size of an element and its position relative to the viewport.
+    // console.log(rect, item.id);
+    if (rect.top > -top && rect.top < top) {
+      top = rect.top;
+      currentElem = item;
+    }
   }
-}
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("header");
-console.log(navbar)
-var sticky = header.offsetTop;
-console.log(sticky)
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  //highlight the section on the viewport and disable the rest of sections
+  //usung classList methods to change the CSS
+  for (let item of navMenu) {
+    if (item.dataset.nav == currentElem.id) {
+      item.classList.add("menu-active");
+    } else {
+      item.classList.remove("menu-active");
+    }
   }
-}
-checkIfSectionInView();
+  currentElem.classList.add("your-active-class");
+  for (item of sections) {
+    if (item.id != currentElem.id) {
+      item.classList.remove("your-active-class");
+    }
+  }
+};
+sectionCreator();
+nav.addEventListener("click", clicker);
+window.addEventListener("scroll", isInViewport);
